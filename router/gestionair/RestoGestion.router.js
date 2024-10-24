3d3ee245b42b34263effce2f3641c03122859caa
OSMaben <freegiftcardishere@gmail.com>
2024-10-24 08:57:01 +0100

    adding  the delette of the resto

--- router/gestionair/RestoGestion.router.js
+++ router/gestionair/RestoGestion.router.js
@@ -5,18 +5,32 @@ const restoValidation = require('../../validation/gestionair/ValidateRestoCreati
 const  ManagerMiddleware = require('../../middleware/managerMiddleware')
 const upload = require('../../util/upload');
 const {AddMenuImages}  = require('../../controller/gestionair/gestionair.controller')
-const {UpdateResto} = require("../../controller/gestionair/gestionair.controller");
+const {UpdateResto,UpdatingMenu,DeleteMenu} = require("../../controller/gestionair/gestionair.controller");
+const VerifyToken = require("../../middleware/VerifyToken");
+
+
+
 //creation of the resto
 router.post('/createResto', upload.fields([
     { name: 'logo', maxCount: 1 },
     { name: 'image_banner', maxCount: 1 }
-]),ManagerMiddleware, CreateResto);
-
-//adding the menu of the resto
-router.post('/addingMenuImages', ManagerMiddleware, upload.single('image'), AddMenuImages);
-
-
+]),ManagerMiddleware, VerifyToken, restoValidation,CreateResto);
 
 //update of the resto
-router.post('/updateResto', ManagerMiddleware, UpdateResto);
+router.post('/updateResto', ManagerMiddleware,VerifyToken, UpdateResto);
+
+//-==============================================
+
+
+//adding the menu of the resto
+router.post('/addingMenuImages', VerifyToken,ManagerMiddleware, upload.single('image'), AddMenuImages);
+
+//updating the menu of the resto
+router.post('/updatingMenu', VerifyToken,ManagerMiddleware, UpdatingMenu)
+
+
+//delete of the resto
+router.post("/deleteMenu",VerifyToken,ManagerMiddleware, DeleteMenu );
+
+
 module.exports = router;
