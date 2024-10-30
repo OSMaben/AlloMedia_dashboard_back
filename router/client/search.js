115c2359e68fc28d5b2dc697b3e14eebc61489a6
fakihiya <yasinfakihi21@gmail.com>
2024-10-30 11:33:11 +0100

    feat: Add methode get all restarant

--- router/client/search.js
+++ router/client/search.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { SearchRestaurants } = require('../../controller/clients/SearchController');
+const { SearchRestaurants, getRestaurants } = require('../../controller/clients/SearchController');
 const { getRestaurantById } = require('../../controller/clients/restaurantMenuController');
 const { addItemToCart } = require('../../controller/clients/cartController');
 const { placeOrder, getOrderStatus } = require('../../controller/clients/orderController');
 const { getRestaurantDetails } = require('../../controller/clients/SearchController');
+const {  getRes } = require('../../controller/clients/SearchController');
 
 router.get('/search', SearchRestaurants);
 router.get('/:id', getRestaurantById);
@@ -12,5 +13,6 @@ router.post('/add', addItemToCart);
 router.get('/restaurants/:id', getRestaurantDetails);
 router.post('/place-order', placeOrder);
 router.get("/track/:orderId", getOrderStatus);
+router.get("/",  getRes);
 
 module.exports = router;
